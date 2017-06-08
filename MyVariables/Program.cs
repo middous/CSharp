19206c966409ae17f3635c34b2a6613c9810ad33
mo sa <middous@hotmail.com>
2017-06-08 01:20:29 -0700

    add comment

---
+++ MyVariables/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyVariables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int myValue = 7;
+            Console.WriteLine("hi {0}",myValue);
+        }
+    }
+}
