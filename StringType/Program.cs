a8cbdb7477e0c0639a99675bdff1feb1a308d3ec
mo sa <middous@hotmail.com>
2017-06-15 03:09:20 -0700

    string Type

---
+++ StringType/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringType
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myString1 = "yes it is";
+            string myString = @" This is my\v String\\Thisis\ agood courxse\n";
+            string myString2 = myString1 + myString;
+            
+            Console.WriteLine("{0}", myString2);
+        }
+    }
+}
