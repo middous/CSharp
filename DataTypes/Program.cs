cce4b8bcd38c4e723a234a95d7ebb8a5d72d4d6f
mo sa <middous@hotmail.com>
2017-06-15 01:34:57 -0700

    data types

---
+++ DataTypes/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int CharMain = Char.MinValue;
+            int CharMax = Char.MaxValue;
+
+            char myVar = 'G';
+            int myVarVal = 'G';
+            Char myVaral = Convert.ToChar(71);
+            Console.WriteLine("M c = {0}, M C = {1} My C = {2} my Va n 3 = {3} my Conver var {4}", CharMain, CharMax, myVar, myVarVal, myVaral);
+            Console.ReadKey();
+        }
+    }
+}
